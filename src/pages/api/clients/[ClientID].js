6b81e67ac9e68e5cec503686ec4e87ba9e6b4098
rafaalmeida1 <rafaempresarial312@gmail.com>
2022-10-07 14:29:05 -0300

    remove setHeader

--- src/pages/api/clients/[ClientID].js
+++ src/pages/api/clients/[ClientID].js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
                 if (!name, !email) throw "invalid data"
                 await Client.updateOne({ _id: ClientID }, { name, email })
                 res.setHeader('Content-Type', 'application/json');
-                res.setHeader('Cache-Control', 'max-age=180000')
+                // res.setHeader('Cache-Control', 'max-age=180000')
                 res.status(200).json({ sucess: true })
             } catch (err) {
                 console.log(err)
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
             try {
                 await Client.deleteOne({_id:ClientID})
                 res.setHeader('Content-Type', 'application/json');
-                res.setHeader('Cache-Control', 'max-age=180000')
+                // res.setHeader('Cache-Control', 'max-age=180000')
                 res.status(201).json({ sucess: true})
             } catch (err) {
                 console.log(err)
--- src/pages/api/clients/index.js
+++ src/pages/api/clients/index.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
             try {
                 const clients = await Client.find({});
                 res.setHeader('Content-Type', 'application/json');
-                res.setHeader('Cache-Control', 'max-age=180000')
+                // res.setHeader('Cache-Control', 'max-age=180000')
                 res.status(200).json({ sucess: true, data: clients })
             } catch (err) {
                 console.log(err)
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
                 if (!name, !email) throw "invalid data"
                 const client = await Client.create({name, email})
                 res.setHeader('Content-Type', 'application/json');
-                res.setHeader('Cache-Control', 'max-age=180000')
+                // res.setHeader('Cache-Control', 'max-age=180000')
                 res.status(201).json({ sucess: true, data: client })                
             } catch (err) {
                 console.log(err)
